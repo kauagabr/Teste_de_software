9f26777f9b395f6ca9fa68a180969e04e2d3e298
kauagabr <kaua32475@gmail.com>
2024-11-05 19:03:08 -0300

    Exemplo de teste de software do capítulo 1 e 2 utilizando jUnit.

---
+++ untitled1/src/main/java/org/example/Leilao.java
@@ -0,0 +1,41 @@
+package org.example;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class Leilao {
+    private String descricao;
+    private List<Lance> lances;
+
+    public Leilao(String descricao) {
+        this.descricao = descricao;
+        this.lances = new ArrayList<Lance>();
+    }
+
+    public void propoe(Lance lance) {
+        if(lances.isEmpty() || (!ultimoLance().getUsuario().equals(lance.getUsuario()) && qtdLances(lance.getUsuario()) < 5)) {
+            lances.add(lance);
+        }
+    }
+
+    private Lance ultimoLance(){
+        return lances.get(lances.size()-1);
+    }
+
+    private int qtdLances(Usuario usuario) {
+        int total = 0;
+        for (Lance lance : lances) {
+            if(lance.getUsuario().equals(usuario)) total++;
+        }
+        return total;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public List<Lance> getLances() {
+        return Collections.unmodifiableList(lances);
+    }
+}
---
+++ untitled1/src/main/java/org/example/Usuario.java
@@ -0,0 +1,13 @@
+package org.example;
+
+public class Usuario {
+    private String nome;
+
+    public Usuario(String nome) {
+        this.nome = nome;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+}
